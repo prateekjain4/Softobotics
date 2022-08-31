622b09da672caf0b7626a2447cb5b566d8b53a92
prateekjain4 <56712397+prateekjain4@users.noreply.github.com>
2022-08-31 09:56:58 +0530

    Create home_navbar.js

---
+++ src/navbar/home_navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function HomeAppBar() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            MOVIE BOOKING APP
+          </Typography>
+          <Button color="inherit" onClick={() => navigate("/signup")}>
+            Register
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/")}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
